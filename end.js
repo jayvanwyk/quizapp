8374981586d98274d2a4a94f0fc20708d6ac00ee
Jay Van Wyk <jayvanwyk@MacBook-Pro.local>
2023-05-27 11:51:36 +0100

    Step 8 Complete

--- end.js
+++ end.js
@@ -1,12 +1,29 @@
 const submitButton = document.getElementById('saveScoreButton');
 const usernameText = document.getElementById('username');
+const finalScoreElement = document.getElementById('finalScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const latestScore = localStorage.getItem('mostRecentScore');
+if(latestScore){
+    finalScoreElement.innerText = latestScore;
+}else{
+    finalScoreElement.innerText = '-'
+}
 
 usernameText.addEventListener('keyup', (e) =>{
-    console.log(e.key);
     submitButton.disabled  = !usernameText.value;
 });
 
 saveHighScore = e => {
     e.preventDefault();
+    const score = {
+        score: latestScore,
+        name: usernameText.value
+    }
+    highScores.push(score);
+    highScores.sort((a, b)=>  b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
 }
 
